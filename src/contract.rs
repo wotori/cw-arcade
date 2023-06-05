5b6cf100a2818cadf48a0076be9c482b77269077
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-06-05 22:00:54 +0300

    another test example added

--- src/contract.rs
+++ src/contract.rs
@@ -49,10 +49,23 @@ mod tests {
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
 
     use super::*;
-    use crate::msg::{GreetResp, InstantiateMsg};
+    use crate::msg::{GreetResp, InstantiateMsg, QueryMsg};
 
     #[test]
     fn greet_query() {
+        // simple test
+        let resp = query::greet().unwrap();
+        assert_eq!(
+            resp,
+            msg::GreetResp {
+                message: "Hello, world!".to_owned()
+            }
+        );
+    }
+
+    #[test]
+    fn greet_query2() {
+        // more comprehensive test
         let mut deps = mock_dependencies();
         let env = mock_env();
         let inst_msg = InstantiateMsg {
@@ -70,6 +83,43 @@ mod tests {
 
         let resp = query(deps.as_ref(), env, msg::QueryMsg::Greet {}).unwrap();
         let resp: GreetResp = from_binary(&resp).unwrap();
+
+        assert_eq!(
+            resp,
+            GreetResp {
+                message: "Hello, world!".to_owned()
+            }
+        );
+    }
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{App, ContractWrapper, Executor};
+
+    #[test]
+    fn greet_query3() {
+        // the most comprehensive test
+        let mut app = App::default();
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let instantiate_msg = InstantiateMsg {
+            arcade: "pacman".to_string(),
+            admin: "owner".to_string(),
+        };
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &instantiate_msg,
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: GreetResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::Greet {})
+            .unwrap();
 
         assert_eq!(
             resp,
