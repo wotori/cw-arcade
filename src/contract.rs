331ca83429f73db6f9a5e44252fe407f30fddfa5
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-06-05 21:31:57 +0300

    more comprehensive test

--- src/contract.rs
+++ src/contract.rs
@@ -45,23 +45,30 @@ mod query {
 
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::{
-        from_binary,
-        testing::{mock_dependencies, mock_env},
-    };
-
-    use crate::msg::GreetResp;
+    use cosmwasm_std::from_binary;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
 
     use super::*;
+    use crate::msg::{GreetResp, InstantiateMsg};
 
     #[test]
     fn greet_query() {
-        let resp = query(
-            mock_dependencies().as_ref(),
-            mock_env(),
-            msg::QueryMsg::Greet {},
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let inst_msg = InstantiateMsg {
+            arcade: "Pac-Man".to_string(),
+            admin: "Wotori".to_string(),
+        };
+
+        instantiate(
+            deps.as_mut(),
+            env.clone(),
+            mock_info("sender", &[]),
+            inst_msg,
         )
         .unwrap();
+
+        let resp = query(deps.as_ref(), env, msg::QueryMsg::Greet {}).unwrap();
         let resp: GreetResp = from_binary(&resp).unwrap();
 
         assert_eq!(
