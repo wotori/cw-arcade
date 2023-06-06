31265cec55f55aa002855c1d4056d8428f1bcaf9
Wotori Movako <10486621+wotori@users.noreply.github.com>
2023-06-06 13:17:57 +0300

    remove unnecessary tests

--- src/contract.rs
+++ src/contract.rs
@@ -1,6 +1,6 @@
 use crate::{
     error::ContractError,
-    msg::{ExecuteMsg, GreetResp, InstantiateMsg, QueryMsg},
+    msg::{ExecuteMsg, InstantiateMsg, QueryMsg},
     state::{ADMINS, ARCADE},
 };
 use cosmwasm_std::{
@@ -108,89 +108,10 @@ mod query {
 
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::from_binary;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-
     use super::*;
-    use crate::msg::{AdminsListResp, GreetResp, InstantiateMsg, QueryMsg};
-
-    #[test]
-    fn greet_query() {
-        // simple test
-        let resp = query::greet().unwrap();
-        assert_eq!(
-            resp,
-            GreetResp {
-                message: "Hello, world!".to_owned()
-            }
-        );
-    }
-
-    #[test]
-    fn greet_query2() {
-        // more comprehensive test
-        let mut deps = mock_dependencies();
-        let env = mock_env();
-        let inst_msg = InstantiateMsg {
-            arcade: "Pac-Man".to_string(),
-            admins: vec!["wotori".to_string(), "senlin".to_string()],
-        };
-
-        instantiate(
-            deps.as_mut(),
-            env.clone(),
-            mock_info("sender", &[]),
-            inst_msg,
-        )
-        .unwrap();
-
-        let resp = query(deps.as_ref(), env, QueryMsg::Greet {}).unwrap();
-        let resp: GreetResp = from_binary(&resp).unwrap();
-
-        assert_eq!(
-            resp,
-            GreetResp {
-                message: "Hello, world!".to_owned()
-            }
-        );
-    }
+    use crate::msg::{AdminsListResp, InstantiateMsg, QueryMsg};
     use cosmwasm_std::Addr;
     use cw_multi_test::{App, ContractWrapper, Executor};
-
-    #[test]
-    fn greet_query3() {
-        // the most comprehensive test
-        let mut app = App::default();
-        let code = ContractWrapper::new(execute, instantiate, query);
-        let code_id = app.store_code(Box::new(code));
-
-        let instantiate_msg = InstantiateMsg {
-            arcade: "pacman".to_string(),
-            admins: vec!["wotori".to_string(), "senlin".to_string()],
-        };
-        let addr = app
-            .instantiate_contract(
-                code_id,
-                Addr::unchecked("owner"),
-                &instantiate_msg,
-                &[],
-                "Contract",
-                None,
-            )
-            .unwrap();
-
-        let resp: GreetResp = app
-            .wrap()
-            .query_wasm_smart(addr, &QueryMsg::Greet {})
-            .unwrap();
-
-        assert_eq!(
-            resp,
-            GreetResp {
-                message: "Hello, world!".to_owned()
-            }
-        )
-    }
 
     #[test]
     fn instantiation() {
